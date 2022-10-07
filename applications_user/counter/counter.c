5e0ae38810a2865882409b6582e63b1d09efbd0d
MixaShap <1226964@gmail.com>
2022-10-07 17:25:57 +0300

    Forgot to stage

--- applications_user/counter/counter.c
+++ applications_user/counter/counter.c
@@ -1,18 +1,24 @@
 #include <furi.h>
 #include <gui/gui.h>
+#include <input/input.h>
+
 
 typedef struct {
     Gui* gui;
     ViewPort* view_port;
-    osMessageQueueId_t event_queue;
+    FuriMessageQueue* event_queue;
     InputEvent event;
     uint16_t counter;
 } CounterApp;
 
-void counter_input_callback(InputEvent* input_event, void* ctx) {
-    furi_assert(ctx);
-    osMessageQueuePut((osMessageQueueId_t)ctx, input_event, 0, 0);
+
+static void counter_input_callback(InputEvent* input_event, FuriMessageQueue* event_queue) {
+    furi_assert(event_queue);
+
+//    PluginEvent event = {.type = EventTypeKey, .input = *input_event};
+furi_message_queue_put(event_queue, input_event, 0);
 }
+
 
 void counter_draw_callback(Canvas* canvas, void* ctx) {
     CounterApp* app = (CounterApp*)ctx;
@@ -29,31 +35,29 @@ void counter_draw_callback(Canvas* canvas, void* ctx) {
     canvas_draw_str(canvas, 15, 60, "Long press back for exit");
 }
 
+
 void counter_app_free(CounterApp* app) {
     gui_remove_view_port(app->gui, app->view_port);
     view_port_free(app->view_port);
-    osMessageQueueDelete(app->event_queue);
+    furi_message_queue_free(app->event_queue);
     furi_record_close("gui");
     free(app);
 }
 
-CounterApp* counter_app_alloc() {
-    CounterApp* app = furi_alloc(sizeof(CounterApp));
-    app->view_port = view_port_alloc();
-    app->gui = furi_record_open("gui");
-    app->event_queue = osMessageQueueNew(8, sizeof(InputEvent), NULL);
-    gui_add_view_port(app->gui, app->view_port, GuiLayerFullscreen);
-    view_port_draw_callback_set(app->view_port, counter_draw_callback, app);
-    view_port_input_callback_set(app->view_port, counter_input_callback, app->event_queue);
-    return app;
-}
 
 int32_t counter_app(void* p) {
     UNUSED(p);
-    CounterApp* app = counter_app_alloc();
+
+    CounterApp* app = furi_alloc(sizeof(CounterApp));
+    app->view_port = view_port_alloc();
+    app->gui = furi_record_open("gui");
+    app->event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
+    gui_add_view_port(app->gui, app->view_port, GuiLayerFullscreen);
+    view_port_draw_callback_set(app->view_port, counter_draw_callback, app);
+    view_port_input_callback_set(app->view_port, counter_input_callback, app->event_queue);
 
     while(1) {
-        furi_check(osMessageQueueGet(app->event_queue, &app->event, NULL, osWaitForever) == osOK);
+        furi_check(furi_message_queue_get(app->event_queue, &app->event, 100) == FuriStatusOk);
         if(app->event.type == InputTypeLong && app->event.key == InputKeyBack) {
             break;
         }
